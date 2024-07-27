6f5f99c9f0867af7916f09d532dd9e22c6024a18
GuglielmoColombo <guglielmo.colombo@toyota-europe.com>
2024-07-27 20:23:57 +0200

    increase time

--- src/components/RecordTrip.js
+++ src/components/RecordTrip.js
@@ -1,11 +1,16 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Container, Button, Alert, Row, Col } from 'react-bootstrap';
 
+var options = {
+  enableHighAccuracy: true,
+  timeout: 1500,
+  maximumAge: 0
+};
+
 const GeolocationRecorder = () => {
   const [locations, setLocations] = useState([]);
   const [error, setError] = useState(null);
   const [isRecording, setIsRecording] = useState(false);
-  const [totalDistance, setTotalDistance] = useState(0);
   const intervalRef = useRef(null);
 
   const getCurrentPosition = () => {
@@ -23,36 +28,15 @@ const GeolocationRecorder = () => {
         setError(null);
       }, (error) => {
         setError("Error obtaining location: " + error.message);
-      });
+      }, options);
     } else {
       setError("Geolocation is not supported by this browser.");
     }
   };
 
-  const calculateSpeed = (distance, time) => {
-    const speedKmh = (distance / time) * 3.6
-    
-    return speedKmh;
-};
-
-  const calculateDistance = (lat1, lon1, lat2, lon2) => {
-    const toRad = (value) => (value * Math.PI) / 180;
-    const R = 6371000; // Radius of the Earth in meters
-    const dLat = toRad(lat2 - lat1);
-    const dLon = toRad(lon2 - lon1);
-    const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c;
-    return distance; // Distance in meters
-};
-
-
   const startRecording = () => {
     setIsRecording(true);
-    intervalRef.current = setInterval(getCurrentPosition, 1500); // Start the interval and store its ID in intervalRef
+    intervalRef.current = setInterval(getCurrentPosition, 2000); // Start the interval and store its ID in intervalRef
   };
 
   const stopRecording = () => {
