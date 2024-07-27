45db793eb80b072d10fb71b520014c30f03fd0da
GuglielmoColombo <guglielmo.colombo@toyota-europe.com>
2024-07-27 13:55:58 +0200

    only tack feature

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter as Router, Link } from 'react-router-dom';
-import CreateTrip from './components/CreateTrip';
-import MyPastTrips from './components/MyPastTrips';
-import ExploreTrips from './components/ExploreTrips';
 import RecordTrip from './components/RecordTrip';
-import NavigationButtons from './components/NavigationButtons';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ExploreTripsMap from './components/ExploreTripsMap';
 
 
 const App = () => {
 
   return (
-    <Router>
-      <>
-        <Routes>
-          <Route path="/" element={<NavigationButtons />} />
-          <Route path="/create-trips" element={<RecordTrip />} />
-          <Route path="/explore-trips" element={<ExploreTrips />} />
-          <Route path="/explore-trips/:filter" element={<ExploreTripsMap/>} />
-          <Route path="/my-past-trips" element={<MyPastTrips />} />
-        </Routes>
-      </>
-    </Router>
-
+    <RecordTrip></RecordTrip>
   );
 };
 
